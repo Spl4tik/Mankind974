4b6dd87aee401f1c3d206e34f6c05bd6421a87dc
Spl4tik <Splatik@mail.ru>
2025-06-24 23:00:18 +0300

    Add new JKO222

---
+++ src/JKO.java
@@ -0,0 +1,8 @@
+public class JKO extends Cooler {
+    @Override
+    public void doCooCoo() {
+        CooCoo += 1550;
+        CooQP = CooCoo * 10;
+        System.out.println("DOING COOOOCOOOOfffffffff " + CooCoo + " 10 Coo + COQP10:" + CooQP);
+    }
+}
