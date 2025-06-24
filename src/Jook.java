36c6824a139822e8c771bec5a86a30db6d900f2f
Spl4tik <Splatik@mail.ru>
2025-06-24 21:54:51 +0300

    Удалена ненужная папка out/production/Mankind974

--- src/Jook.java
+++
@@ -1,5 +0,0 @@
-public class Jook {
-    public String ASKER;
-
-
-}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("QQQQQ");
-        Jook jookOne = new Jook();
-       jookOne.ASKER="Askme";
-        System.out.println(jookOne.ASKER);
-    }
-}
