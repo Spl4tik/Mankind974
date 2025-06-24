cd9b58bad7479c0c1722ae832d2e716fa7f6fea8
Spl4tik <Splatik@mail.ru>
2025-06-25 00:02:09 +0300

    Новый день 00:01

--- src/Javelin.java
+++ src/Javelin.java
@@ -1,6 +1,6 @@
 public class Javelin {
     public static void main(String[] args) {
-        System.out.println("Nn");
+        System.out.println("Nna");
         Cooler coolerFirst = new Cooler();
         coolerFirst.doCooCoo();
         coolerFirst.doCooCoo();
